431781001a11e48cf2663b41774bf46a106b63ed
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-15 09:48:03 -0500

    Add Array Exercise

---
+++ Arrays.php
@@ -0,0 +1,8 @@
+<?php
+$numbers = array (0, 1, 2, 3, 4, 5);
+var_dump($numbers);
+print_r($numbers);
+$numbers = ['1', '2', '3', '4', '5'];
+echo $numbers[3] . PHP_EOL;
+$person = array('info' => array('first_name' => 'Matthew', 'last_name' => 'Martinez', 'email' => 'myname@gmail.com', 'phone' => '817.294.4365'));
+print_r($person);
