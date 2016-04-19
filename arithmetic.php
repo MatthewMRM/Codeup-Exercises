3cc687e66eee42054264b8b6593a73915dca7a73
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-19 14:28:33 -0500

    Arithmetic Exercise- Added functions and passing them

---
+++ arithmetic.php
@@ -0,0 +1,32 @@
+<?php
+
+function add($a, $b)
+{
+    return $a + $b;
+}
+
+function subtract($a, $b)
+{
+    return $a - $b;
+    // Add code here
+}
+
+function multiply($a, $b)
+{
+    return $a * $b;
+    // Add code here
+}
+
+function divide($a, $b)
+{
+    return $a / $b;
+    // Add code here
+}
+
+echo add(10,5) . PHP_EOL;
+echo subtract(10, 5) . PHP_EOL;
+echo multiply(10,5) . PHP_EOL;
+echo divide(10,5) . PHP_EOL;
+
+
+// Add code to test your functions here
