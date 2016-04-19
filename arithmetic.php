e806cfcfdfac6ce416d006e8cc89200f9a58ca16
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-19 15:52:49 -0500

    Added error messages and error message for dividing by zero

--- arithmetic.php
+++ arithmetic.php
@@ -1,11 +1,36 @@
 <?php
+
+function validateNumber($a, $b)
+{
+	return is_numeric($a) && is_numeric($b);
+}
+function validateNotZero($b)
+{
+	return $b == 0; 
+}
+
+function errorMessage($a, $b, $errortype)
+{
+	switch ($errortype) {
+		case 'nonNumeric':
+			echo "Error! You must enter two numbers, you entered {$a} and {$b} ";
+			break;
+		
+		case 'divideByZero':
+			echo "Error, you can't divide by zero! You entered {$a} and {$b} ";
+			break;
+		default:
+			echo "Error not found!";
+			break;
+	}
+}		
 // Functions listed below to call upon the variables
 function add($a, $b)
 {
 	if (is_numeric($a) && is_numeric($b)) {
         return $a + $b;
     } else {
-        return "ERROR: Both arguments must be numbers\n";
+        return errorMessage($a, $b, nonNumeric);
     }
 }
 
@@ -14,7 +39,7 @@ function subtract($a, $b)
 	if (is_numeric($a) && is_numeric($b)) {
         return $a - $b;
     } else {
-        return "ERROR: Both arguments must be numbers\n";
+        return errorMessage($a, $b, nonNumeric);
     }
 }
 
@@ -23,16 +48,18 @@ function multiply($a, $b)
 	if (is_numeric($a) && is_numeric($b)) {
         return $a * $b;
     } else {
-        return "ERROR: Both arguments must be numbers\n";
+        return errorMessage($a, $b, nonNumeric);
     }
 }
 
 function divide($a, $b)
 {
-	if (is_numeric($a) && is_numeric($b)) {
-        return $a / $b;
-    } else {
-        return "ERROR: Both arguments must be numbers\n";
+	if(!validateNumber($a, $b)){
+		errorMessage($a, $b, 'nonNumeric');
+	} else if(validateNotZero($b)){
+		errorMessage($a, $b, 'divideByZero');
+	} else {
+    	return $a / $b;
     }
 }
 
@@ -41,15 +68,15 @@ function modulus($a, $b)
 	if (is_numeric($a) && is_numeric($b)) {
         return $a % $b;
     } else {
-        return "ERROR: Both arguments must be numbers\n";
+        return errorMessage($a, $b, nonNumeric);
     }
 }
 
 // My variables listed that are called upon
-echo add(10,5) . PHP_EOL;
+echo add(10,w) . PHP_EOL;
 echo subtract(10, 5) . PHP_EOL;
 echo multiply(10,5) . PHP_EOL;
-echo divide(10,5) . PHP_EOL;
+echo divide(10,0) . PHP_EOL;
 echo modulus(10,5) . PHP_EOL;
 
 // Add code to test your functions here
