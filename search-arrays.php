493b447be6891bf6e610f94ff1df43a087f6091c
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-20 14:12:40 -0500

    Added Search Array and Compare Array

--- search-arrays.php
+++ search-arrays.php
@@ -4,5 +4,25 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-$result = array_search('Sarah', $names);
-var_dump($result);
+
+function searchArray($query, $names)
+{
+	if(array_search($query, $names) !== false){
+		return "TRUE" . PHP_EOL;
+	} else {
+		return "FALSE" . PHP_EOL;
+	} 
+}
+function compareArray($names, $compare) 
+{
+	$match = 0;
+	foreach ($names as $name) {
+		if(array_search($name, $compare) !== false)
+		{
+			$match++;
+		}
+	}	
+	return $match;
+}
+echo searchArray($compare, $names) . PHP_EOL;
+echo compareArray($names,$compare) . PHP_EOL;
