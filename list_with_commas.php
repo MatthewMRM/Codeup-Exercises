ee9c7415ab24ab0a3b21c69d007a3747529d7656
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-21 14:12:12 -0500

    Added function humanizedList, added sorting by alphbetical order.

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,20 +1,20 @@
 <?php
-
- // Converts array into list n1, n2, ..., and n3
- function humanizedList($array) {
-   // Your solution goes here!
- }
-
- // List of famous peeps
- $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
-
- // TODO: Convert the string into an array
- $physicistsArray = [];
-
- // Humanize that list
- $famousFakePhysicists = humanizedList($physicistsArray);
-
- // Output sentence
- echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
-
- ?>
+function humanizedList($array, $sort = false) 
+{
+	if($sort == true) {
+		asort($array);
+	}
+	$lastValue = array_pop($array);
+	$arrayString = implode(', ', $array);
+	$arrayString = $arrayString . ' and ' . $lastValue;
+	return $arrayString;
+}
+// List of famous peeps
+$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+// TODO: Convert the string into an array
+$physicistsArray = explode(', ', $physicistsString);
+// Humanize that list
+$famousFakePhysicists = humanizedList($physicistsArray, true);
+// Output sentence
+echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}." . PHP_EOL;
+?>
