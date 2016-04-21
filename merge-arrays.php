11968976a17ccb8f59b3499c09feca4826ecb829
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-21 09:16:46 -0500

    Initial commit

---
+++ merge-arrays.php
@@ -0,0 +1,26 @@
+<?php
+$names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
+$compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+function inArray($name, $array)
+{
+	if (array_search($name, $array) !== false){
+		return TRUE;
+	} else {
+		return FALSE;
+	}
+	
+}
+function compareArrays($name1, $compare2)
+{	
+	$count = 0;
+	foreach($name1 as $name){
+		if (array_search($name, $compare2) !== false){
+			$count++;
+		}	
+	}
+	return $count; 
+}
+var_dump(inArray('Tina',$names)) . PHP_EOL;
+var_dump(inArray('Bob',$names)) . PHP_EOL;
+echo compareArrays($names, $compare) . PHP_EOL;
