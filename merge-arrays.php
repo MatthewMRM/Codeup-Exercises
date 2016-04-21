15b22fa8ad06ecadbbbe45001be6ba892ac4e923
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-21 10:23:34 -0500

    Merge Array exercise

--- merge-arrays.php
+++ merge-arrays.php
@@ -1,26 +1,41 @@
 <?php
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
-
-function inArray($name, $array)
+function search($name, $array){
+    $result = array_search($name, $array);
+    if($result !== false) {
+        return true;
+    } else {
+        return false;
+    }
+}
+function compareArrays($arrayOne, $arrayTwo){
+    $count = 0;
+    foreach($arrayOne as $name){
+        if(search($name, $arrayTwo)){
+            var_dump($name);
+            $count++;
+        }
+    }
+    return $count;
+}
+function combineArrays($arrayOne, $arrayTwo)
 {
-	if (array_search($name, $array) !== false){
-		return TRUE;
-	} else {
-		return FALSE;
-	}
-	
+    $newArray = [];
+    foreach ($arrayOne as $person1) {
+       $comparison = array_shift($arrayTwo);
+       if ($person1 == $comparison) {
+           array_push($newArray, $person1);
+       } else{
+            array_push($newArray, $person1, $comparison);
+       }
+    }
+    return $newArray;
 }
-function compareArrays($name1, $compare2)
-{	
-	$count = 0;
-	foreach($name1 as $name){
-		if (array_search($name, $compare2) !== false){
-			$count++;
-		}	
-	}
-	return $count; 
-}
-var_dump(inArray('Tina',$names)) . PHP_EOL;
-var_dump(inArray('Bob',$names)) . PHP_EOL;
-echo compareArrays($names, $compare) . PHP_EOL;
+print_r(combineArrays($names, $compare)) ;
+
+
+echo "There are " . compareArrays($names, $compare) . " items in the array that match." . PHP_EOL;
+var_dump(search('Tina', $names));
+
+
