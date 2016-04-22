22f47efe6782b975db8c42f8722aa82fd8687c73
Matthew Martinez <mrmartinez8689@gmail.com>
2016-04-22 10:24:44 -0500

    Added explode, foreach to create key and value, Created value, as well as adding dashes (substr)

--- contact-parser.php
+++ contact-parser.php
@@ -5,22 +5,28 @@ function parseContacts($filename)
 	$contacts = array();
 	$filename = 'contacts.txt';
 	$handle = fopen($filename, 'r');
-	$contents = fread($handle, filesize($filename));
+	$contentString = trim(fread($handle, filesize($filename)));
     $arrayOfStrings = explode(PHP_EOL, $contentString);
 
     // todo - read file and parse contacts
     foreach ($arrayOfStrings as $index => $contact) {
     	$innerArray = explode('|' , $contact);
-    			$contactsArray[$index] ['name'] = $innerArray[0];
-    			$contactsArray[$index] ['number'] = numberSplit($innerArray[1]);
+    	$contactsArray[$index] ['name'] = $innerArray[0];
+    	$innerArray[1] = parseNumber($innerArray[1]);
+    	$contactsArray[$index] ['number'] = $innerArray[1];
     }
+    
+
+    
     fclose($handle);
     return $contactsArray;
-}
 
-function numberSplit($number)
-{
-	return substr($number, 0, 3) . '-' . substr($number, 6);start)
 }
+function parseNumber($string){
+$phoneNum = substr($string, 0, 3) . '-' . substr($string, 3, 3) . '-' . substr($string, 6);
 
-var_dump(parseContacts('contacts.txt'));
+return $phoneNum;
+}
+print_r(parseContacts('contacts.txt'));
+
+
