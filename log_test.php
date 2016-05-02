7471e714aa1127ebe115fc556386297d30558c99
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-02 14:29:16 -0500

    Added Cli to my test

--- log_test.php
+++ log_test.php
@@ -1,5 +1,10 @@
 <?php
-require_once 'log.php';
-$log = new Log();
+require_once 'Log.php';
+$log = new Log('cli'); // its right here. Create an instance. 
+$logger = new Log(); // i really did. another new instance.
+
+$log = new Log;
+
 $log->info('hello');
+$log->error('Womp Womp');
 
