09f949ab2874a23f7ea796fc7a167180dca112bf
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-02 11:13:03 -0500

    Inital commit

---
+++ Log.php
@@ -0,0 +1,37 @@
+
+<?php
+class Log 
+{
+	 public $filename;
+	 public $handle;
+	
+	
+	
+	}
+	public function logMessage($logLevel, $message) 
+	{
+		$time = date("H-i-s");
+		
+		$this->date = date("Y-m-d");
+		
+	    $this->filename =   "{date}.log";
+	    $date = date("Y-m-d");
+	    
+	   $onScreen = "Todays date: {$date}  The Current Time: {$time} " . $logLevel . " " . $message . PHP_EOL;
+		fwrite($this->handle,"{$onScreen}" . PHP_EOL);
+	
+		
+	}
+	public function info($message) 
+	{
+	$this->logMessage("INFO", $message);
+	}
+	public function error($message) 
+	{
+	$this->logMessage("ERROR", $message);
+	}
+	public function __destruct()
+	{
+		fclose($this->handle);
+	}
+}
