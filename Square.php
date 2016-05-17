2fba1b46cdf124452b58028ea5c07721293562b8
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-17 14:01:44 -0500

    Added set/get

--- Square.php
+++ Square.php
@@ -1,18 +1,25 @@
-
 <?php
 class Square extends Rectangle
 {
     public function __construct($height)
     {
-        parent::__construct($height, $height);
+         $this->setHeight($height);
     } 
+    public function setHeight($height)
+    {
+        $this->height = $height;
+    }
+    public function getHeight()
+    {
+        return $this->height;
+    }
     public function area() 
     {
-        return $this->height * 4;
+        return $this->height * 2;
     }
     public function perimeter()
     {
-        return ($this->height * 2) + ($this->width * 2);
+        return $this->height * 4;
     }
 };
 ?>
