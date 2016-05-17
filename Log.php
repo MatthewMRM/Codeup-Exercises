66cb444a871e2d25692f532a11112731add0cc3c
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-17 14:57:22 -0500

    updated

--- Log.php
+++ Log.php
@@ -1,26 +1,48 @@
+
 <?php
-class Log {
-    public function __construct($prefix='log') {
-        $date = date("Y-m-d");
-        $this->filename = "$prefix-$date.log";
+//set correct timezone
+date_default_timezone_set("America/Chicago");
+class Log
+{
+    protected $filename;
+    protected $handle;
+   
+    public function __construct($prefix = 'log')
+    {
+        $currentDate = date('Y-m-d');
+       
+        $this->setFilename("{$prefix}-{$currentDate}.log");
         $this->handle = fopen($this->filename, 'a');
     }
-    public function __destruct() {
+    public function setFilename($filename)
+    {
+        if(is_string($filename) && touch($filename) && is_writable($filename)) {
+            $this->filename = $filename;
+        } else {
+            echo "Error.";
+        }
+    }
+    
+    public function logMessage($logLevel, $message)
+    {
+        //setting variable for correct time/date format
+        $currentDateTime = date('Y-m-d h:i:s=T');
+        fwrite($this->handle, PHP_EOL . $currentDateTime . " " . "[" . $logLevel ."]" . " " . $message);
+    }
+    public function info($message) 
+    {
+        $this->logMessage('INFO', $message);
+    }
+    public function error($message)
+    {
+        $this->logMessage("ERROR", $message);
+    }
+    public function __destruct()
+    {
         fclose($this->handle);
     }
-    public $filename;
-    public $handle;
-    public function logMessage($logLevel, $message){
-        $today = date("Y-m-d H:i:s");
-        fwrite($this->handle, $today . ' ' . $logLevel . ' ' . $message . PHP_EOL);
-    }
-    public function info($message){
-        $this->logMessage('[INFO]', $message);
-    }
-    public function error($message){
-        $this->logMessage('[ERROR]', $message);
-    }
 }
+?>
 
 
 
