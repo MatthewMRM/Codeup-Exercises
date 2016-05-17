3144a1a8801a797ce6beaf79bd67b782ebb5c460
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-17 10:20:36 -0500

    added var_dump

--- shapes_test.php
+++ shapes_test.php
@@ -4,12 +4,15 @@ REQUIRE_ONCE 'Rectangle.php';
 REQUIRE_ONCE 'Square.php';
 $Rectangle = new Rectangle(20, 40);
 echo 'Rectangle One area is: ' . $Rectangle->area() . PHP_EOL;
+var_dump($Rectangle->area());
 $Rectangle = new Rectangle(3, 100);
 echo 'Rectangle Two area is: ' . $Rectangle->area() . PHP_EOL;
 $Square = new Square(2, 2);
 echo 'Square area is: ' . $Square->area() . PHP_EOL;
+var_dump($Square->area());
 echo 'Square perimeter is: ' . $Square->perimeter() . PHP_EOL;
 $Square = new Square(8, 8);
 echo 'Square area is: ' . $Square->area() . PHP_EOL;
+var_dump($Square->area());
 echo 'Square perimeter is: ' . $Square->perimeter() . PHP_EOL;
 ?>
