9e12627833c797f9bbb63d1d5fb48c0686fc1ba2
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-17 14:00:21 -0500

    added get to and setters

--- Rectangle.php
+++ Rectangle.php
@@ -1,22 +1,35 @@
 <?php
-
 class Rectangle
 {
-	private $height;
-	private $width;
-}
-
-public function __construct($height, $width)
-{
-        $this->$height = $height;
-        $this->$width  = $width;
-}
-
-public function area()
-{
-	return $this->$height * $this->$width;
-}
-
+    private $height;
+    private $width;
+    public function __construct($height, $width)
+    {
+        // $this->height = $height;
+        // $this->width = $width;
+        $this->setHeight($height);
+        $this->setWidth($width);
+    } 
+    public function setHeight($height)
+    {
+        $this->height = $height;
+    }
+    public function setWidth($width)
+    {
+        $this->width = $width;
+    }
+    public function getHeight()
+    {
+        return $this->height;
+    }
+    public function getWidth()
+    {
+        return $this->width;
+    }
+    public function area() 
+    {
+        return $this->height * $this->width;
+    }
      public function perimeter()
     {
         return ($this->height * 2) + ($this->width * 2);
