c2aa67ea23f489fbd3d4e3402ed415821d1b0618
Matthew Martinez <mrmartinez8689@gmail.com>
2016-05-17 11:23:04 -0500

    added parent constructor

--- Square.php
+++ Square.php
@@ -2,7 +2,16 @@
 <?php
 class Square extends Rectangle
 {
-    public function perimeter() {
+    public function __construct($height)
+    {
+        parent::__construct($height, $height);
+    } 
+    public function area() 
+    {
+        return $this->height * 4;
+    }
+    public function perimeter()
+    {
         return ($this->height * 2) + ($this->width * 2);
     }
 };
